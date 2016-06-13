f7dc12a0ef2a6cd85cf1723900cd3008b47fa8a4
random-person-001 <rileylegos6@gmail.com>
2016-06-13 16:50:09 -0500

    Work on inventory.  Now has correct (but unused) selector and puts thing in spots nicely.

--- SourceryTextb1/GameObjects/Inventory.java
+++ SourceryTextb1/GameObjects/Inventory.java
@@ -7,6 +7,7 @@ import SourceryTextb1.art;
 
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.util.Arrays;
 
 
 /**
@@ -18,22 +19,22 @@ class Inventory{
     private ImageOrg org;
     private Layer invBkgdLayer;
     private Layer itemsLayer;
+    private art arty = new art();
     private String layerName = "Inventory";
-    private int selectX = 0;
-    private int selectY = 0;
+    private int selectX = 1;
+    private int selectY = 1;
     private int[][] arrayOfStuff; // TODO make the array of things in inventory
 
     public Inventory(ImageOrg orgo, Player p){
         org = orgo;
         player = p;
-        art arty = new art();
     }
 
     public void show() {
         player.frozen = true;
         invBkgdLayer = new Layer(art.strToArray(new art().inventoryBkgd), layerName, false, true);
         org.addLayer(invBkgdLayer); // Background grid
-        populateItems();
+        itemsLayer = populateItems();
         org.addLayer(itemsLayer);
         Layer selector = new Layer(new String[22][46], "selector", false, false);
         org.addLayer(selector); // Foreground stuff
@@ -43,6 +44,7 @@ class Inventory{
         window.txtArea.addKeyListener(keyListener); // Add key listeners.
         while (!keyListener.resume){
             try {
+                updateSelector();
                 org.compileImage();
                 Thread.sleep(50);
             } catch (InterruptedException e) {
@@ -56,46 +58,88 @@ class Inventory{
         player.frozen = false;
     }
 
-    private void populateItems(){
-        itemsLayer = new Layer(new String[22][46], "invItems", false, false);
+    private Layer populateItems(){
+        Layer itemsLayer = new Layer(new String[22][47], "invItems", false, false);
         if (player.inventory.containsKey("Book") && player.inventory.get("Book")>0){
-            putItem(new art().oldBook, 1,1);
+            putItem(itemsLayer, arty.oldBook, 1, 1);
+            putItem(itemsLayer, arty.oldBook, 4, 3);
+            putItem(itemsLayer, arty.oldBook, 2, 5);
         }
+        if (player.inventory.containsKey("Spark") && player.inventory.get("Spark")>0){
+            putItem(itemsLayer, arty.spark, 1, 2);
+        }
+        return itemsLayer;
+    }
+    private void updateSelector(){
+        int indexY = 4*(selectY-1) + 1;
+        int indexX = 8*(selectX-1);
+        System.out.println("X: " + indexX + "     Y:" + indexY);
+        org.clearLayer("selector");
+        org.editLayer("|", "selector", indexY, indexX);
+        org.editLayer("|", "selector", indexY+1, indexX);
+        org.editLayer("|", "selector", indexY+2, indexX);
+        org.editLayer("|", "selector", indexY, indexX+7);
+        org.editLayer("|", "selector", indexY+1, indexX+7);
+        org.editLayer("|", "selector", indexY+2, indexX+7);
+        org.compileImage();
     }
 
-    private void putItem (String thing, int indexX, int indexY){
+    private void putItem (Layer lay, String thing, int indexX, int indexY){
+        indexY = 4*(indexY-1) + 1;
+        indexX = 8*(indexX-1) + 1;
         String[][] arrThing = art.strToArray(thing);
-        for (int i = 0; i<arrThing[0].length; i++){
-            for (int j = 0; j<arrThing.length; j++){
-                org.editLayer(arrThing[j][i], "invItems", j+indexY, i+indexX);
+        System.out.println(Arrays.deepToString(arrThing));
+        for (int i = 0; i<arrThing.length; i++){
+            for (int j = 0; j<arrThing[0].length; j++){
+                lay.setStr(i+indexY, j+indexX, arrThing[i][j]);
             }
         }
     }
-
-    String invenetoryBkgd =
-                    "///..........  I N V E N T O R Y  .........\\\\\\\n" +
-                    ".      ..       ..      ..      ..      ..   .\n" +
-                    ".      ..       ..      ..      ..      ..   .\n" +
-                    ".      ..       ..      ..      ..      ..   .\n" +
-                    "..........................................   .\n" +
-                    ".      ..       ..      ..      ..      ..   .\n" +
-                    ".      ..       ..      ..      ..      ..   .\n" +
-                    ".      ..       ..      ..      ..      ..   .\n" +
-                    "..........................................   .\n" +
-                    ".      ..       ..      ..      ..      ..   .\n" +
-                    ".      ..       ..      ..      ..      ..   .\n" +
-                    ".      ..       ..      ..      ..      ..   .\n" +
-                    "..........................................   .\n" +
-                    ".      ..       ..      ..      ..      ..   .\n" +
-                    ".      ..       ..      ..      ..      ..   .\n" +
-                    ".      ..       ..      ..      ..      ..   .\n" +
+    private void putPrimary (String thing){
+        int y = 19;
+        int x = 30;
+        String[][] arrThing = art.strToArray(thing);
+        for (int i = 0; i<arrThing.length; i++){
+            for (int j = 0; j<arrThing[0].length; j++){
+                org.editLayer(arrThing[i][j], "invItems", i+y, j+x);//4,4);//i+y, j+x);
+            }
+        }
+    }
+    private void putSecondary (String thing){
+        int y = 19;
+        int x = 38;
+        String[][] arrThing = art.strToArray(thing);
+        for (int i = 0; i<arrThing.length; i++){
+            for (int j = 0; j<arrThing[0].length; j++){
+                org.editLayer(arrThing[i][j], "invItems", i+y, j+x);
+            }
+        }
+    }
+    String inveuntoryBkgd =
+                    "///.........  I N V E N T O R Y  ..........\\\\\\\n" +
+                    ".      ..      ..      ..      ..      ..    .\n" +
+                    ".      ..      ..      ..      ..      ..    .\n" +
+                    ".      ..      ..      ..      ..      ..    .\n" +
+                    ".........................................    .\n" +
+                    ".      ..      ..      ..      ..      ..    .\n" +
+                    ".      ..      ..      ..      ..      ..    .\n" +
+                    ".      ..      ..      ..      ..      ..    .\n" +
+                    ".........................................    .\n" +
+                    ".      ..      ..      ..      ..      ..    .\n" +
+                    ".      ..      ..      ..      ..      ..    .\n" +
+                    ".      ..      ..      ..      ..      ..    .\n" +
+                    ".........................................    .\n" +
+                    ".      ..      ..      ..      ..      ..    .\n" +
+                    ".      ..      ..      ..      ..      ..    .\n" +
+                    ".      ..      ..      ..      ..      ..    .\n" +
                     "..............................................\n" +
-                    ".      ..       ..      ..|..EQUIPPED SPELLS..\n" +
-                    ".      ..       ..      ..|....1st......2nd...\n" +
-                    ".      ..       ..      ..|...      ..      ..\n" +
+                    ".      ..      ..      ...|..EQUIPPED SPELLS..\n" +
+                    ".      ..      ..      ...|....1st......2nd...\n" +
+                    ".      ..      ..      ...|...      ..      ..\n" +
                     "..........................|...      ..      ..\n" +
                     "..........................|...      ..      ..\n" +
                     "\\\\\\........................................///\n";
+
 
     private int getObject(int x,int y){
         return arrayOfStuff[x][y];
@@ -105,27 +149,32 @@ class Inventory{
         System.out.println(c);
         switch (c){
             case '©': // Up
-                selectY++;
+                if (selectY > 1)
+                    selectY--;
                 break;
             case '®': // Down
-                selectY--;
+                if (selectY < 5 && !(selectX >= 4 && selectY == 4 ))
+                    selectY++;
                 break;
             case 'µ': // Left
-                selectX--;
+                if (selectX > 1)
+                    selectX--;
                 break;
             case 'æ': // Right
-                selectX++;
+                if (selectX < 5 && !(selectY == 5 && selectX == 3 ))
+                    selectX++;
                 break;
             case '1': // Set primary
-                player.setPrimarySpell("Test");
+                putPrimary(arty.spark);
+                player.setPrimarySpell("Spark");
                 break;
             case '2': // Set secondary
-                player.setSecondarySpell("Second");
+                putSecondary(arty.oldBook);
+                player.setSecondarySpell("Book");
                 break;
             default:
                 break;
         }
-        org.compileImage();
     }
 
 }
@@ -133,7 +182,7 @@ class Inventory{
 
 class Navigator extends KeyAdapter {
     private Inventory inv;
-    public boolean resume = false;
+    boolean resume = false;
 
     Navigator(Inventory inventory) {
         inv = inventory;
