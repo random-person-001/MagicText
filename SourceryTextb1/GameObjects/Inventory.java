936bc61b71481469dd6a9740394bdf0e65d81b1e
random-person-001 <rileylegos6@gmail.com>
2016-06-16 11:35:05 -0500

    New inventory is half-built.  I don't see how we'll get as many items in here, though.

--- SourceryTextb1/GameObjects/Inventory.java
+++ SourceryTextb1/GameObjects/Inventory.java
@@ -15,7 +15,7 @@ import java.util.HashMap;
  * A class to select something in your inventory
  * Created by riley on 12-Jun-2016.
  */
-class Inventory{
+class Inventory {
     private HashMap<String, Integer> inventory;
     private Player player;
     private ImageOrg org;
@@ -26,8 +26,9 @@ class Inventory{
     private String layerName = "WindowConfig";
     private int selectX = 1;
     private int selectY = 1;
+    private int scrollTimer = 0;
 
-    public Inventory(ImageOrg orgo, Player p){
+    public Inventory(ImageOrg orgo, Player p) {
         org = orgo;
         player = p;
         inventory = new HashMap<>(); // I have no idea why this can't go at a class-level with the declaration.
@@ -44,54 +45,331 @@ class Inventory{
         //putPrimary(arty.oldBook);  // Wouldn't work, not sure why.
     }
 
-    /** Add another of an item in the inventory
+    /**
+     * Add another of an item in the inventory
+     *
      * @param itemName which one?
      */
-    public void addItem(String itemName){
-        if (inventory.containsKey(itemName)){
+    public void addItem(String itemName) {
+        if (inventory.containsKey(itemName)) {
             int n = inventory.get(itemName);
-            inventory.put(itemName, n+1);
-        }else{
-            inventory.put(itemName,1);
+            inventory.put(itemName, n + 1);
+        } else {
+            inventory.put(itemName, 1);
         }
     }
 
-    /** Make there be one less item in the player's inventory
+    /**
+     * Make there be one less item in the player's inventory
+     *
      * @param itemName which to have less of
      */
-    public void subtractItem(String itemName){
-        if (inventory.containsKey(itemName)){
+    public void subtractItem(String itemName) {
+        if (inventory.containsKey(itemName)) {
             int n = inventory.get(itemName);
-            inventory.put(itemName, n-1);
-        }else{
+            inventory.put(itemName, n - 1);
+        } else {
             inventory.put(itemName, 0);
         }
     }
 
-    /** How many of <X> do I have in my inventory?  Find out!
+    /**
+     * How many of <X> do I have in my inventory?  Find out!
+     *
      * @param itemName which item
      * @return how many of itemName s there are
      */
-    public int getItem(String itemName){
-        if (inventory.containsKey(itemName)){
+    public int getItem(String itemName) {
+        if (inventory.containsKey(itemName)) {
             return inventory.get(itemName);
-        }else{
+        } else {
             inventory.put(itemName, 0);
             return 0;
         }
     }
-    /** Do I have <X> in my inventory?  Find out!
+
+    /**
+     * Do I have <X> in my inventory?  Find out!
+     *
      * @param itemName which item
      * @return whether there are more than 0
      */
-    public boolean hasItem(String itemName){
+    public boolean hasItem(String itemName) {
         return getItem(itemName) > 0;
     }
 
 
     /**
+     * @return the item name of what is selected
+     */
+    private String getSelectedName() {
+        return getSelected(1);
+    }
+
+    /**
+     * @return A shortish description of what is selected
+     */
+    private String getSelectedDescription() {
+        return getSelected(2);
+    }
+
+//    For reference.  This actually doesn't do anything here
+//
+//                    ///.........  I N V E N T O R Y  ..........\\\
+//                    .      ..      ..      ..      ..      ..    .
+//                    .      ..      ..      ..      ..      ..    .
+//                    .      ..      ..      ..      ..      ..    .
+//                    .........................................    .
+//                    .      ..      ..      ..      ..      ..    .
+//                    .      ..      ..      ..      ..      ..    .
+//                    .      ..      ..      ..      ..      ..    .
+//                    .........................................    .
+//                    .      ..      ..      ..      ..      ..    .
+//                    .      ..      ..      ..      ..      ..    .
+//                    .      ..      ..      ..      ..      ..    .
+//                    .........................................    .
+//                    .      ..      ..      ..      ..      ..    .
+//                    .      ..      ..      ..      ..      ..    .
+//                    .      ..      ..      ..      ..      ..    .
+//                    ..............................................
+//                    .      ..      ..      ...|..EQUIPPED SPELLS..
+//                    .      ..      ..      ...|....1st......2nd...
+//                    .      ..      ..      ...|...      ..      ..
+//                    ..........................|...      ..      ..
+//                    ..........................|...      ..      ..
+//                    \\\........................................///
+
+
+    /**
+     * Press a key?  Call this to do stuff!
+     *
+     * @param c which character you have pressed on the board
+     */
+    void keyPressed(char c) {
+        System.out.println(c);
+        switch (c) {
+            case '©': // Up
+                if (selectY > 1)
+                    selectY--;
+                scrollTimer = 0;
+                break;
+            case '®': // Down
+                if (selectY < 8)
+                    selectY++;
+                scrollTimer = 0;
+                break;
+            case '1': // Set primary
+                putPrimary(getSelectedName());
+                player.setPrimarySpell(getSelectedName());
+                break;
+            case '2': // Set secondary
+                putSecondary(getSelectedName());
+                player.setSecondarySpell(getSelectedName());
+                break;
+            default:
+                break;
+        }
+    }
+
+    /**
      * Bring up the inventory for the player to do stuff with.
      */
+    public void show() {
+        player.frozen = true;
+        invBkgdLayer = new Layer(art.strToArray(new art().inventoryBkgd), layerName, false, true);
+        org.addLayer(invBkgdLayer); // Background grid
+        itemsLayer = populateItems();
+        org.addLayer(itemsLayer);
+        org.addLayer(selectedSpellsLayer);
+        Layer selector = new Layer(new String[22][46], "selector", false, false);
+        org.addLayer(selector); // Foreground stuff
+
+        Window window = org.getWindow();
+        Navigator keyListener = new Navigator(this);
+        window.txtArea.addKeyListener(keyListener); // Add key listeners.
+        while (!keyListener.resume) {
+            try {
+                updateSelector();
+                org.compileImage();
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                System.exit(0);
+            }
+        }
+        org.removeLayer("selector");
+        org.removeLayer(selectedSpellsLayer.name);
+        org.removeLayer("invItems");
+        org.removeLayer(layerName);
+        window.txtArea.removeKeyListener(keyListener);
+        player.frozen = false;
+    }
+
+    private Layer populateItems() {
+        Layer itemsLayer = new Layer(new String[22][47], "invItems", false, false);
+        if (hasItem("Book")) {
+            putItem(itemsLayer, "Book", 1);
+        }
+        if (hasItem("Spark")) {
+            putItem(itemsLayer, "Spark", 2);
+        }
+        if (hasItem("Flame")) {
+            putItem(itemsLayer, "Flame", 3);
+        }
+        if (hasItem("Wanderer")) {
+            putItem(itemsLayer, "Wanderer", 4);
+        }
+        if (hasItem("SmallHealth")) {
+            putItem(itemsLayer, "Small Health", 5);
+        }
+        if (hasItem("HugeHealth")) {
+            putItem(itemsLayer, "Huge Health", 6);
+        }
+        return itemsLayer;
+    }
+
+    private void putItem(Layer lay, String name, int indexY) {
+        int yCoord = 2 * indexY;
+        int xCoord = 4;
+        char[] arrThing = name.toCharArray(); // Add name
+        for (int i = 0; i < arrThing.length; i++) {
+            lay.setStr(yCoord, i + xCoord, String.valueOf(arrThing[i]));
+        }
+        xCoord = 15;
+        String desrciption = getInfo(2, indexY); // Add description
+        arrThing = desrciption.toCharArray();
+        for (int i = 0; i < arrThing.length; i++) {
+            lay.setStr(yCoord, i + xCoord, String.valueOf(arrThing[i]));
+        }
+    }
+
+    private void updateSelector() {
+        System.out.println(scrollTimer);
+        int indexY = 2 * selectY;
+        int indexX = 2;
+        System.out.println("X: " + selectX + "     Y:" + selectY);
+        org.clearLayer("selector");
+        org.editLayer("@", "selector", indexY, indexX);
+
+        //Scroll description sideways
+        int xCoord = 15;
+        int yCoord = indexY;
+        String desrciption = getInfo(2, selectY); // Add description
+        char[] arrThing = (" "+desrciption).toCharArray();
+        scrollTimer ++;
+        if (scrollTimer > arrThing.length){
+            scrollTimer = 0;
+        }
+        Layer lay = org.getLayer(org.getPosLayer("invItems"));
+        for (int i = 0; i < arrThing.length; i++) {
+            int x = i + xCoord - scrollTimer;
+            if (x >= 15) {
+                lay.setStr(yCoord, x, String.valueOf(arrThing[i]));
+            }
+        }
+
+        org.compileImage();
+    }
+
+    private String getSelected(int what){
+        return getInfo(what, selectY);
+    }
+    /**
+     * Fetch-all method to get random stuff from what's selected currently.  Consider using the more intuitively
+     * named getSelectedArt(), getSelectedName(), or getSelectedDescription().
+     *
+     * @param what a magical integer that specifies which kind of thing to fetch
+     * @return a String thing corresponding to what you asked for.
+     */
+    private String getInfo(int what, int yIndex) {
+        if (yIndex == 1 && hasItem("Book")) {
+            if (what == 1) {
+                return "Book";
+            } else if (what == 2) {
+                return "This is an old book you found in your pocket.  You can't read it, so it's probably worth more as a weapon.";
+            }
+            return arty.oldBook;
+        } else if (yIndex == 1 && hasItem("Spark")) {
+            if (what == 1) {
+                return "Spark";
+            } else if (what == 2) {
+                return "A small spell you found after killing a pot of petunias";
+            }
+            return arty.spark;
+        } else if (yIndex == 3 && hasItem("Flame")) {
+            if (what == 1) {
+                return "Flame";
+            } else if (what == 2) {
+                return "A little spell that helps along the spontaneous reaction of an enemy with oxygen to form CO2 and H2O.";
+            }
+            return arty.flame;
+        } else if (yIndex == 4 && hasItem("Wanderer")) {
+            if (what == 1) {
+                return "Wanderer";
+            } else if (what == 2) {
+                return "A respectable spell that wanders around and inflicts decent damage.  Not aimable, may hit you.";
+            }
+            return arty.wanderer;
+        } else if (yIndex == 5 && hasItem("SmallHealth")) {
+            if (what == 1) {
+                return "SmallHealth";
+            } else if (what == 2) {
+                return "You can regenerate a little health with this.";
+            }
+            return arty.smallHealth;
+        } else if (yIndex == 6 && hasItem("HugeHealth")) {
+            if (what == 1) {
+                return "HugeHealth";
+            } else if (what == 2) {
+                return "A huge boost to your health at a huge mana cost";
+            }
+            return arty.hugeHealth;
+        } else {
+            if (what == 1) {
+                return "None";
+            } else if (what == 2) {
+                return "";
+            }
+            return arty.emptyItem;
+        }
+    }
+
+
+    /**
+     * Put a new item art in the Primary Spell place
+     *
+     * @param thing a String (not String[][], this does that!).
+     */
+    private void putPrimary(String thing) {
+        int y = 19;
+        int x = 30;
+        char[] arrThing = thing.toCharArray();
+        for (int i = 0; i < arrThing.length; i++) {
+                org.editLayer(String.valueOf(arrThing[i]), selectedSpellsLayer.name, y, i + x);
+        }
+    }
+
+    /**
+     * Put a new item art in the Secondary Spell place
+     *
+     * @param thing a String (not String[][], this does that!).
+     */
+    private void putSecondary(String thing) {
+        int y = 19;
+        int x = 38;
+        char[] arrThing = thing.toCharArray();
+        for (int i = 0; i < arrThing.length; i++) {
+            org.editLayer(String.valueOf(arrThing[i]), selectedSpellsLayer.name, y, i + x);
+        }
+    }
+
+
+    /// OLD INVENTORY
+
+
+    /*
+    /**
+     * Bring up the inventory for the player to do stuff with.
+     *
     public void show() {
         player.frozen = true;
         invBkgdLayer = new Layer(art.strToArray(new art().inventoryBkgd), layerName, false, true);
@@ -162,22 +440,8 @@ class Inventory{
     }
 
     /**
-     * @return the item name of what is selected
-     */
-    private String getSelectedName(){
-        return getSelected(1);
-    }
-
-    /**
-     * @return A shortish description of what is selected
-     */
-    private String getSelectedDescription(){
-        return getSelected(2);
-    }
-
-    /**
      * @return the art (String, not String[][]) of the item currently selected
-     */
+     *
     private String getSelectedArt(){
         return getSelected(3);
     }
@@ -186,7 +450,7 @@ class Inventory{
      * named getSelectedArt(), getSelectedName(), or getSelectedDescription().
      * @param what a magical integer that specifies which kind of thing to fetch
      * @return a String thing corresponding to what you asked for.
-     */
+     *
     private String getSelected(int what) {
         if (selectX == 1 && selectY == 1 && hasItem("Book")) {
             if (what == 1) {
@@ -255,7 +519,7 @@ class Inventory{
     /**
      * Put a new item art in the Primary Spell place
      * @param thing a String (not String[][], this does that!).
-     */
+     *
     private void putPrimary (String thing){
         int y = 19;
         int x = 30;
@@ -269,7 +533,7 @@ class Inventory{
     /**
      * Put a new item art in the Secondary Spell place
      * @param thing a String (not String[][], this does that!).
-     */
+     *
     private void putSecondary (String thing){
         int y = 19;
         int x = 38;
@@ -279,68 +543,7 @@ class Inventory{
                 org.editLayer(arrThing[i][j], selectedSpellsLayer.name, i+y, j+x);
             }
         }
-    }
-//    For reference.  This actually doesn't do anything here
-//
-//                    ///.........  I N V E N T O R Y  ..........\\\
-//                    .      ..      ..      ..      ..      ..    .
-//                    .      ..      ..      ..      ..      ..    .
-//                    .      ..      ..      ..      ..      ..    .
-//                    .........................................    .
-//                    .      ..      ..      ..      ..      ..    .
-//                    .      ..      ..      ..      ..      ..    .
-//                    .      ..      ..      ..      ..      ..    .
-//                    .........................................    .
-//                    .      ..      ..      ..      ..      ..    .
-//                    .      ..      ..      ..      ..      ..    .
-//                    .      ..      ..      ..      ..      ..    .
-//                    .........................................    .
-//                    .      ..      ..      ..      ..      ..    .
-//                    .      ..      ..      ..      ..      ..    .
-//                    .      ..      ..      ..      ..      ..    .
-//                    ..............................................
-//                    .      ..      ..      ...|..EQUIPPED SPELLS..
-//                    .      ..      ..      ...|....1st......2nd...
-//                    .      ..      ..      ...|...      ..      ..
-//                    ..........................|...      ..      ..
-//                    ..........................|...      ..      ..
-//                    \\\........................................///
-
-
-    /** Press a key?  Call this to do stuff!
-     * @param c which character you have pressed on the board
-     */
-    void keyPressed(char c){
-        System.out.println(c);
-        switch (c){
-            case '©': // Up
-                if (selectY > 1)
-                    selectY--;
-                break;
-            case '®': // Down
-                if (selectY < 5 && !(selectX >= 4 && selectY == 4 ))
-                    selectY++;
-                break;
-            case 'µ': // Left
-                if (selectX > 1)
-                    selectX--;
-                break;
-            case 'æ': // Right
-                if (selectX < 5 && !(selectY == 5 && selectX == 3 ))
-                    selectX++;
-                break;
-            case '1': // Set primary
-                putPrimary(getSelectedArt());
-                player.setPrimarySpell(getSelectedName());
-                break;
-            case '2': // Set secondary
-                putSecondary(getSelectedArt());
-                player.setSecondarySpell(getSelectedName());
-                break;
-            default:
-                break;
-        }
-    }
+    }*/
 
 }
 
