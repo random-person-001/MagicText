4084ec767578474bc64124634007e850269b5402
random-person-001 <rileylegos6@gmail.com>
2016-09-18 23:04:32 -0500

    Fix bad merging.

--- SourceryTextb1/GameObjects/LockedDoor.java
+++ SourceryTextb1/GameObjects/LockedDoor.java
@@ -5,27 +5,8 @@ import SourceryTextb1.Layer;
 import SourceryTextb1.Rooms.Room;
 
 /**
+ * A locked door!  Needs a key (or lock picking) to unlock
  * Created by Jared on 17-Sep-16.
- */
-public class LockedDoor extends GameObject{
-    String keyName;
-    int lockpickDifficulty;
-    String layerName;
-
-    public LockedDoor(ImageOrg org, Room container, String nameOfKey, int xSet, int ySet, int difficulty){
-        x = xSet;
-        y = ySet;
-        keyName = nameOfKey;
-        lockpickDifficulty = difficulty;
-        orgo = org;
-        room = container;
-        strClass = "LockedDoor";
-
-        layerName = room.makeUniqueLayerName(strClass);
-        Layer selfLayer = new Layer(new String[1][1], layerName, y, x);
-        selfLayer.setStr(0,0, ":");
-        orgo.addLayer(selfLayer);
- * Created by Jared on 09-Sep-16.
  */
 public class LockedDoor extends GameObject{
     private String layerName;
