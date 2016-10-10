51f1321d4794f1f8605def0bea7935acb03eb992
HazilTheNut <hurin615@gmail.com>
2016-10-10 18:04:12 -0500

    Made water look better, waves don't update all at once.  So unnatural...

--- SourceryTextb1/GameObjects/WaterPool.java
+++ SourceryTextb1/GameObjects/WaterPool.java
@@ -15,25 +15,36 @@ public class WaterPool extends GameObject{
     private Layer puddle;
     private boolean[][] puddleData;
 
+    private int [] waveXs;
+    private int [] waveYs; //Welcome to wavey's, how can I take your order?
+    private int [] waveTimers;
+
     public WaterPool(Room creator, Layer set, ImageOrg org, int xSet, int ySet){
         puddle = set;
+        int puddleSize = 0;
         puddleData = new boolean[puddle.getColumns()][puddle.getRows()];
         for (int c = 0; c < puddle.getColumns(); c++){
             for (int r = 0; r < puddle.getRows(); r++){
                 if (puddle.getStr(r,c).equals("W")) {
                     puddleData[c][r] = true;
                     puddle.setSpecTxt(r, c, new SpecialText(" ", null, new Color(65, 65, 200)));
+                    puddleSize++;
                 }
             }
         }
+        int waveCount = (puddleSize / 10) + 2; //Scales number of waves present with size of pool
+        waveXs = new int[waveCount];
+        waveYs = new int[waveCount];
+        waveTimers = new int[waveCount];
+
         String debugOutput = "[Water] Boolean matrix:\n";
 
         for (int r = 0; r < puddleData[0].length; r++){
             for (int c = 0; c < puddleData.length ; c++){
                 if (puddleData[c][r]){
-                    debugOutput += "1";
+                    debugOutput += "~";
                 } else {
-                    debugOutput += "0";
+                    debugOutput += "#";
                 }
             }
             debugOutput += "\n";
@@ -55,17 +66,19 @@ public class WaterPool extends GameObject{
     public void update(){
         // Waves move across surface
         Random rand = new Random();
-        if (time/100 % 6 == 0){
-            for (int r = 0; r<puddle.getRows(); r++){
-                for (int c = 0; c<puddle.getColumns(); c++){
-                    if (puddleData[c][r]) {
-                        if (rand.nextBoolean()) {
-                            puddle.setSpecTxt(r, c, new SpecialText("~", new Color(0, 117, 200), new Color(65, 65, 200)));
-                        } else {
-                            puddle.setSpecTxt(r, c, new SpecialText(" ", null, new Color(65, 65, 200)));
-                        }
-                    }
+        for (int ii = 0; ii < waveTimers.length ; ii++){
+            if (waveTimers[ii] == 0){ //Checks for every wave if it supposed to 'despawn'
+                if (puddleData[waveXs[ii]][waveYs[ii]]){ //First off removes the wave from where it is now
+                    puddle.setSpecTxt(waveYs[ii], waveXs[ii], new SpecialText(" ", null, new Color(65, 65, 200)));
                 }
+                waveTimers[ii] = rand.nextInt(6) + 2; //Then gives the wave a new (pseudo-random) time to wait before 'despawning' again
+                waveXs[ii] = rand.nextInt(puddleData.length); //Creates new x and y coords for pool
+                waveYs[ii] = rand.nextInt(puddleData[0].length);
+                if (puddleData[waveXs[ii]][waveYs[ii]]){ //Checks if new location is in the pool
+                    puddle.setSpecTxt(waveYs[ii], waveXs[ii], new SpecialText("~", new Color(0, 117, 200), new Color(65, 65, 200)));
+                }
+            } else {
+                waveTimers[ii]--; //Otherwise counts the timer down
             }
         }
         for (Player player : room.players) {
