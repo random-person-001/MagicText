c36295abb205d448a8818cf2644166041b89e2e8
HazilTheNut <hurin615@gmail.com>
2016-11-11 22:22:33 -0600

    Player can sprint once again.

--- SourceryTextb1/GameObjects/Player.java
+++ SourceryTextb1/GameObjects/Player.java
@@ -760,7 +760,7 @@ public class Player extends Mortal implements java.io.Serializable {
                 rightPressed =  true;
             }
             else if (event.getKeyCode() == KeyEvent.VK_SPACE) {
-                upPressed =  true;
+                spacePressed = true;
             }
             else if (event.getKeyCode() == KeyEvent.VK_ESCAPE) {
                 keyPressed('\'');
@@ -780,6 +780,8 @@ public class Player extends Mortal implements java.io.Serializable {
                 leftPressed = false;
             } else if (event.getKeyCode() == KeyEvent.VK_RIGHT) {
                 rightPressed = false;
+            } else if (event.getKeyCode() == KeyEvent.VK_SPACE) {
+                spacePressed = false;
             }
         }
     }
