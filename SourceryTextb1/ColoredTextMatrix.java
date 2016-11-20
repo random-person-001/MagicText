2aca83b50ce837b875c40ab490771a71a605186e
random-person-001 <rileylegos6@gmail.com>
2016-11-20 15:07:48 -0600

    Reduce CPU usage by about a third.  Now the ColoredTextMatrix only recalculates variables - and critically, font - on window resize.
    I didn't remove any features (except the obsolete retrieveColor), I just moved them around.
    
    Note for Linux users: MagicText still makes XOrg cpu usage high, like 60%.  I think it was around 90% earlier, though. (100% -> one core totally busy, I have 4 of 'em')

--- SourceryTextb1/ColoredTextMatrix.java
+++ SourceryTextb1/ColoredTextMatrix.java
@@ -2,37 +2,59 @@ package SourceryTextb1;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
 import java.util.Timer;
 import java.util.TimerTask;
-
-import static java.lang.Math.abs;
 
 /**
  * Created by Jared on 9/25/2016.
  */
-public class ColoredTextMatrix extends JPanel{
+public class ColoredTextMatrix extends JPanel {
     public SpecialText[][] text = new SpecialText[46][28];
+    private boolean recentlyResized = false;
 
-    int HOR_SEPARATION = 9;
-    int VER_SEPARATION = 16;
-    int CHAR_WIDTH     = 10;
-    int CHAR_HEIGHT    = 16;
-    int CHAR_SIZE      = 15;
-    int HOR_MARGIN     = 0;
+    private int HOR_SEPARATION = 9;
+    private int VER_SEPARATION = 16;
+    private int CHAR_WIDTH = 10;
+    private int CHAR_HEIGHT = 16;
+    private int CHAR_SIZE = 15;
+    private int HOR_MARGIN = 0;
 
-    public ColoredTextMatrix(){
+    ColoredTextMatrix() {
         setOpaque(true);
-        setFont(new Font("Monospaced", Font.PLAIN, CHAR_SIZE));
         setFocusable(true);
+        setFont(new Font("Monospaced", Font.PLAIN, CHAR_SIZE));
+        addComponentListener(new ComponentResizeListener());
 
-        Timer timing = new Timer();
+        Timer timing = new Timer("ColoredTextTimer");
         timing.scheduleAtFixedRate(new frameResetTimer(), 25, 25);
     }
 
-    @Override
-    public void paintComponent (Graphics g){
-        g.setColor(Color.BLACK);
-        g.fillRect(0,0, getWidth(), getHeight());
+    private class ComponentResizeListener implements ComponentListener {
+        /**
+         * // Now is a great time to recalculate display variables!
+         * @param e something I don't care about
+         */
+        @Override
+        public void componentResized(ComponentEvent e) {
+            //System.out.println("Window resized!");
+            recalculate();
+        }
+
+        // These aren't important; it refused to run by throwing errors until I implemented these.
+        @Override
+        public void componentMoved(ComponentEvent componentEvent) {}
+        @Override
+        public void componentShown(ComponentEvent componentEvent) {}
+        @Override
+        public void componentHidden(ComponentEvent componentEvent) {}
+    }
+
+    /**
+     * Recalculate all of the display stuff, to call during window resizing (when it changes)
+     */
+    private void recalculate(){
         HOR_SEPARATION = getWidth() / 46; //Calculates horizontal and vertical separation of the letters
         VER_SEPARATION = (getHeight() / 28); //Intentionally wrong
         int adjustedVerSep = (int)((float)VER_SEPARATION * (9f/16)); //Adjusts horizontal separation if too big for vertical.
@@ -47,36 +69,35 @@ public class ColoredTextMatrix extends JPanel{
         CHAR_HEIGHT = CHAR_SIZE + 1;
 
         setFont(new Font("Monospaced", Font.PLAIN, CHAR_SIZE));
+        recentlyResized = true; // Used to redraw grey lines at edges of the useful area of the window
+    }
 
-        if (HOR_MARGIN > 0) {
+    @Override
+    public void paintComponent (Graphics g){
+        //float startTime = System.currentTimeMillis();
+        g.setColor(Color.BLACK);
+        g.fillRect(0,0, getWidth(), getHeight());
+
+        if (recentlyResized && HOR_MARGIN > 0) {
             int secondX = HOR_MARGIN + (HOR_SEPARATION * 46);
             g.setColor(new Color(30, 30, 30));
             g.drawLine(HOR_MARGIN - 1, 0, HOR_MARGIN - 1, getHeight());
             g.drawLine(secondX, 0, secondX, getHeight());
+            recentlyResized = false;
         }
+        //recalculate(); // See the difference it makes by uncommenting - my unscientific methods measured around a thirdish difference
 
-        for (int col = 0; col < text.length; col++){ //Draws the highlighting / backgrounds first
+        for (int col = 0; col < text.length; col++){ //Draws the highlighting / backgrounds first, then the foreground
             for (int row = 0; row < text[0].length; row++){
                 SpecialText get = text[col][row];
                 if (get != null){
-                    g.setColor(get.getBackgroundColor());
+                    g.setColor(get.backgroundColor);
                     g.fillRect((col * HOR_SEPARATION) + HOR_MARGIN, (row * VER_SEPARATION) + (CHAR_SIZE / 5), CHAR_WIDTH, CHAR_HEIGHT);
-                }
-            }
-        }
-        for (int col = 0; col < text.length; col++){ //Afterwards drawing the text on top
-            for (int row = 0; row < text[0].length; row++){
-                SpecialText get = text[col][row];
-                if (get != null){
-                    g.setColor(retrieveColor(get, row, col));
+                    g.setColor(get.foregroundColor);
                     g.drawString(get.getStr(), (col * HOR_SEPARATION) + HOR_MARGIN, (row * VER_SEPARATION) + CHAR_SIZE);
                 }
             }
         }
-    }
-
-    private Color retrieveColor(SpecialText input, int row, int col){
-        return input.foregroundColor;
     }
 
     private class frameResetTimer extends TimerTask {
