74ec9b84993f50def9d5579fbc4f9f25056889b5
random-person-001 <rileylegos6@gmail.com>
2017-02-01 22:35:11 -0600

    Multiplayer should now work with multiple clients.  Except keymaps.

--- SourceryText/SlaveGameInstance.java
+++ SourceryText/SlaveGameInstance.java
@@ -6,28 +6,28 @@ import SourceryText.Network.NetworkServerWorker;
 import java.io.IOException;
 
 /**
- * A slave instance of the multiplayer game.  Exists in own window somewhere else.
+ * A slave instance of the multiplayer game.  This is the local, serverside code that takes care of clients that are connected.
  * Created by riley on 09-Oct-2016.
  */
 public class SlaveGameInstance {
     private GameInstance masterInstance;
+    private Player me;
 
     public SlaveGameInstance(GameInstance master) {
         masterInstance = master;
     }
 
     /**
-     * Set up server and call run game appropriately.  Note that the player actually does
+     * Set up server and call run game appropriately.
      */
     public void runGameAsSlave() {
-        Player me = masterInstance.requestNewPlayer();
-        System.out.println(me.getUsername() + " instantiated.  Now waiting for a connection for them...");
-        try {
-            NetworkServerWorker networkServer = new NetworkServerWorker(me);
-            networkServer.doTimerSend();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        masterInstance.runGame(me);
+        me = masterInstance.requestNewPlayer();
+        System.out.println("[SlaveGameInstance] player " + (me == null ? "null" : "nonnull"));
+        //new Thread(this::runGameAsSlave).start();
+        (new Thread(() -> masterInstance.runGame(me))).start();
+    }
+
+    public Player getCorrespondingPlayer(){
+        return me;
     }
 }
